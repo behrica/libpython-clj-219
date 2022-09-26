903a8bed0a8364215350196dc53ffb985d544f0c
Carsten Behring <carsten.behring@gmail.com>
2022-09-26 10:18:45 +0200

    more complex model

--- train.clj
+++ train.clj
@@ -8,34 +8,52 @@
          '[libpython-clj2.python :refer [py.- py.] :as py])
 
 (println "-------- manual-gil: " ffi/manual-gil)
-
+;; (py/initialize!)
 
 (def gil (ffi/lock-gil))
 
 (def pd (py/import-module "pandas"))
 (def st (py/import-module "simpletransformers.classification"))
 
+(def data
+  (->
+   (arrow/stream->dataset "train.arrow" {:key-fn keyword})
+   (tc/select-columns [:text :labels])))
+
+
+
+(def split
+  (tc/split->seq data))
+
 (def pd-train
-  ((py/py.- pd DataFrame)
-   (->
-    (arrow/stream->dataset "train.arrow" {:key-fn keyword})
-    (tc/select-columns [:text :labels])
-    (tc/head 1)
-    (tc/rows :as-seqs))))
+  (-> split
+      first
+      :train
+      (tc/rows :as-seqs)
+      ((py/py.- pd DataFrame))))
+
+(def pd-eval
+  (-> split
+      first
+      :test
+      (tc/rows :as-seqs)
+      ((py/py.- pd DataFrame))))
+   
 
 
 (def model ((py.- st ClassificationModel)
-            "bert" "prajjwal1/bert-tiny"
-            ;; "electra" "google/electra-base-discriminator"
+            ;; "bert" "prajjwal1/bert-tiny"
+            "electra" "google/electra-base-discriminator"
             :use_cuda true
             :args {:use_multiprocessing false
                    :use_multiprocessing_for_evaluation false
                    :process_count 1
+                   :evaluate_during_training true
                    :overwrite_output_dir true}))
                    
 
 (println "start training")
-(println :result  (py. model train_model pd-train))
+(println :result  (py. model train_model pd-train :eval_df pd-eval))
 
 (println "finished training")
 (ffi/unlock-gil gil)
