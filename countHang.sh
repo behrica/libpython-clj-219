459106b7f7afee8367d62025dcaa84f5fb9480eb
Carsten Behring <carsten.behring@gmail.com>
2022-09-26 22:02:32 +0200

    use require-pythin

--- countHang.sh
+++ countHang.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 rm -f signals.txt
-for i in {1..100}; do timeout -s 9 60 ./train.sh &> /dev/null ; echo $? >> signals.txt; done
+for i in {1..100}; do timeout -s 9 1200 ./train.sh &> /tmp/run.log ; echo $? >> signals.txt; done
 echo "Result: "
 cat signals.txt | sort | uniq -c
--- train.clj
+++ train.clj
@@ -2,10 +2,12 @@
   (:require
    [tablecloth.api :as tc]
    [tech.v3.libs.arrow :as arrow]
-   [libpython-clj2.python.ffi :as ffi]))
+   [libpython-clj2.python.ffi :as ffi]
+   [libpython-clj2.python :refer [py.- py.] :as py]))
 
-(require
-         '[libpython-clj2.python :refer [py.- py.] :as py])
+
+
+
 
 ;; should be "true" for tis code to work, enabled by java property libpython_clj.manual_gil=true
 (println "-------- manual-gil: " ffi/manual-gil)
@@ -16,9 +18,11 @@
 ;; without it, we get JVM crash
 (def gil (ffi/lock-gil))
 
+(require
+  '[libpython-clj2.require :as py-req])
 
-(def pd (py/import-module "pandas"))
-(def st (py/import-module "simpletransformers.classification"))
+(py-req/require-python '[pandas :as pd])
+(py-req/require-python '[simpletransformers.classification :as st])
 
 (def data
   (->
@@ -35,19 +39,18 @@
       first
       :train
       (tc/rows :as-seqs)
-      ((py/py.- pd DataFrame))))
+      (pd/DataFrame)))
 
 (def pd-eval
   (-> split
       first
       :test
       (tc/rows :as-seqs)
-      ((py/py.- pd DataFrame))))
+      (pd/DataFrame)))
    
 
 
-(def model ((py.- st ClassificationModel)
-            ;; "bert" "prajjwal1/bert-tiny"
+(def model (st/ClassificationModel
             "electra" "google/electra-base-discriminator"
             :use_cuda true
             :args {:use_multiprocessing false
