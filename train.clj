af91cf747dc86b1940bc205b4bd7b26a6cbf4afe
Carsten Behring <carsten.behring@gmail.com>
2022-09-26 09:38:05 +0200

    use embbeded

--- train.clj
+++ train.clj
@@ -1,12 +1,16 @@
 (ns train
   (:require
    [tablecloth.api :as tc]
-   [tech.v3.libs.arrow :as arrow]))
+   [tech.v3.libs.arrow :as arrow]
+   [libpython-clj2.python.ffi :as ffi]))
 
 (require
          '[libpython-clj2.python :refer [py.- py.] :as py])
 
-(py/initialize!)
+(println "-------- manual-gil: " ffi/manual-gil)
+
+
+(def gil (ffi/lock-gil))
 
 (def pd (py/import-module "pandas"))
 (def st (py/import-module "simpletransformers.classification"))
@@ -19,17 +23,19 @@
     (tc/head 1)
     (tc/rows :as-seqs))))
 
+
 (def model ((py.- st ClassificationModel)
             "bert" "prajjwal1/bert-tiny"
-
-
+            ;; "electra" "google/electra-base-discriminator"
             :use_cuda true
-            :args {:silent false
+            :args {:use_multiprocessing false
+                   :use_multiprocessing_for_evaluation false
+                   :process_count 1
                    :overwrite_output_dir true}))
+                   
 
 (println "start training")
 (println :result  (py. model train_model pd-train))
 
 (println "finished training")
-;; just in case
-(shutdown-agents)
+(ffi/unlock-gil gil)
---
+++ train.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+export TOKENIZERS_PARALLELISM=false
+export _JAVA_OPTIONS="-Dlibpython_clj.manual_gil=true"
+python -c 'from clojurebridge import cljbridge;cljbridge.load_clojure_file(clj_file="train.clj",mvn_local_repo="/home/carsten/.m2/repository")'
